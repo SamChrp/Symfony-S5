517786ca6c28dcac21cff1c6522eb3e1657f4f01
Sam <100703206+SamChrp@users.noreply.github.com>
2023-09-25 16:16:41 +0200

    wip : add entities + relations + composer require api

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MovieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MovieRepository::class)]
@@ -17,7 +19,24 @@ class Movie
     private ?string $title = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $yearOfRelease = null;
+    private ?string $releaseDate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $duration = null;
+
+    #[ORM\ManyToOne(inversedBy: 'movies')]
+    private ?Category $category = null;
+
+    #[ORM\ManyToMany(targetEntity: Actor::class, inversedBy: 'movies')]
+    private Collection $actor;
+
+    public function __construct()
+    {
+        $this->actor = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -36,14 +55,74 @@ class Movie
         return $this;
     }
 
-    public function getYearOfRelease(): ?string
+    public function getReleaseDate(): ?string
     {
-        return $this->yearOfRelease;
+        return $this->releaseDate;
     }
 
-    public function setYearOfRelease(string $yearOfRelease): static
+    public function setReleaseDate(string $releaseDate): static
     {
-        $this->yearOfRelease = $yearOfRelease;
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDuration(): ?string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(string $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Actor>
+     */
+    public function getActor(): Collection
+    {
+        return $this->actor;
+    }
+
+    public function addActor(Actor $actor): static
+    {
+        if (!$this->actor->contains($actor)) {
+            $this->actor->add($actor);
+        }
+
+        return $this;
+    }
+
+    public function removeActor(Actor $actor): static
+    {
+        $this->actor->removeElement($actor);
 
         return $this;
     }
